3ab90e7f141cae8191c899b755d3ea90acdd97e5
agwermann <alexandrewermann@gmail.com>
2023-10-14 20:34:55 -0300

    chore: adapt lib for local dev

--- device-service/modules/ktwin/event/event.py
+++ device-service/modules/ktwin/event/event.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from ..common import EVENT_TYPE_VIRTUAL_GENERATED, EVENT_TYPE_REAL_GENERATED, get_broker_url, KTwinEvent, build_cloud_event
 from cloudevents.http import to_structured, from_http
@@ -12,10 +13,11 @@ def send_to_real_twin(twin_interface, twin_instance, data):
     cloud_event = build_cloud_event(ce_type, ce_source, data)
     headers, body = to_structured(cloud_event)
 
-    response = requests.post(get_broker_url(), headers=headers, data=body)
+    if os.getenv("ENV") != "local":
+        response = requests.post(get_broker_url(), headers=headers, data=body)
 
-    if response.status_code != 202:
-        raise Exception("Error when pushing to event broker", response)
+        if response.status_code != 202:
+            raise Exception("Error when pushing to event broker", response)
 
 def push_to_virtual_twin(twin_interface, twin_instance, data):
     ce_type = EVENT_TYPE_REAL_GENERATED.format(twin_interface)
@@ -23,10 +25,11 @@ def push_to_virtual_twin(twin_interface, twin_instance, data):
     cloud_event = build_cloud_event(ce_type, ce_source, data)
     headers, body = to_structured(cloud_event)
 
-    response = requests.post(get_broker_url(), headers=headers, data=body)
+    if os.getenv("ENV") != "local":
+        response = requests.post(get_broker_url(), headers=headers, data=body)
 
-    if response.status_code != 202:
-        raise Exception("Error when pushing to event broker", response)
+        if response.status_code != 202:
+            raise Exception("Error when pushing to event broker", response)
 
 def handle_event(request: requests.Request, twin_interface: str, callback):
     ktwin_event = handle_request(request)
