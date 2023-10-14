43db9a9be92c04447fe2cee07f454d6cf834c5d5
agwermann <alexandrewermann@gmail.com>
2023-10-14 19:23:31 -0300

    feat: add get_twin_graph_by_relationship method

--- pole-service/modules/ktwin/command/command.py
+++ pole-service/modules/ktwin/command/command.py
@@ -1,6 +1,6 @@
 import requests
 from ..common import EVENT_TYPE_COMMAND_EXECUTED, build_cloud_event, get_broker_url, KTwinCommandEvent
-from ..twingraph import get_relationship_from_graph, load_twin_graph
+from ..twingraph import get_relationship_from_graph, load_twin_graph, get_twin_graph_by_relationship
 from cloudevents.http import to_structured, from_http
 
 # command: the name of the command that will be invoked in the target interface.
@@ -22,9 +22,15 @@ def execute_command(command: str, command_payload: dict, relationship_name: str,
         raise Exception("Error when pushing to event broker", response)
 
 def handle_command(request: requests.Request, twin_interface: str, command: str, callback):
+    twin_graph = load_twin_graph()
     ktwin_command_event = handle_request(request)
+    target_twin_instance = get_twin_graph_by_relationship(relationship_twin_instance=ktwin_command_event.twin_instance, relationship_twin_interface=ktwin_command_event.twin_interface, twin_graph=twin_graph)
+
+    if target_twin_instance is None:
+        raise Exception("Target twin instance not exists for the following source instance: " + ktwin_command_event.twin_instance)
+
     if ktwin_command_event.twin_interface == twin_interface and ktwin_command_event.command == command:
-        callback(ktwin_command_event)
+        callback(ktwin_command_event, target_twin_instance)
 
 def handle_request(request) -> KTwinCommandEvent:
     cloud_event = from_http(request.headers, request.get_data())
--- pole-service/modules/ktwin/twingraph/twingraph.py
+++ pole-service/modules/ktwin/twingraph/twingraph.py
@@ -35,11 +35,22 @@ def load_twin_graph() -> dict[TwinGraph]:
 
     return twin_graph
 
+# Get the Graph relationship by name and instance
 def get_relationship_from_graph(twin_instance: str, relationship_name: str, twin_graph: dict[str, TwinGraph]) -> TwinReference:
     graph_node = twin_graph[twin_instance]
     
     for relationship in graph_node.relationships:
         if relationship.name == relationship_name:
             return relationship
+        
+    return None
+
+# Get Twin Graph Node by twin instance and interface
+def get_twin_graph_by_relationship(relationship_twin_instance: str, relationship_twin_interface: str, twin_graph: dict[str, TwinGraph]) -> TwinReference:
+    for twin_instance in twin_graph:
+        graph_node = twin_graph[twin_instance]
+        for relationship in graph_node.relationships:
+            if relationship.twin_instance == relationship_twin_instance and relationship.twin_interface == relationship_twin_interface:
+                return graph_node
         
     return None
