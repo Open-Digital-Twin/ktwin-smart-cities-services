f4d301282c054cc6c2344ac9af44a52d10d2fd35
agwermann <alexandrewermann@gmail.com>
2023-10-14 21:15:50 -0300

    chore: apply modules to neighborhood

---
+++ neighborhood-service/modules/ktwin/eventstore/__init__.py
@@ -0,0 +1,9 @@
+from modules.ktwin.eventstore.eventstore import get_event_store_url
+from modules.ktwin.eventstore.eventstore import get_latest_twin_event
+from modules.ktwin.eventstore.eventstore import update_twin_event
+
+__all__ = [
+    "get_event_store_url",
+    "get_latest_twin_event",    
+    "update_twin_event",
+]
---
+++ neighborhood-service/modules/ktwin/eventstore/eventstore.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from ..common import get_event_store_url, KTwinEvent
+from cloudevents.http import from_http, to_binary
+
+# Event Store Methods
+# Interact with event store
+
+def get_latest_twin_event(twin_interface, twin_instance):
+    url = get_event_store_url() + "/api/v1/twin-events/%s/%s/latest" % (twin_interface, twin_instance)
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        return None
+
+    cloud_event = from_http(response.headers, response.content)
+    return KTwinEvent(cloud_event)
+
+def update_twin_event(ktwin_event: KTwinEvent):
+    url = get_event_store_url() + "/api/v1/twin-events"
+    headers, body = to_binary(ktwin_event.cloud_event)
+
+    if os.getenv("ENV") != "local":
+        response = requests.post(url, data=body, headers=headers)
+
+        if response.status_code != 202:
+            raise Exception("Error while updating twin event", response)
