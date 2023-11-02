5c8c6b3fb372e03eee4c24778325486a0df60dbe
agwermann <alexandrewermann@gmail.com>
2023-11-02 20:39:59 -0300

    feat: add handle air quality level

--- neighborhood-service/main.py
+++ neighborhood-service/main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from flask import Flask, request
 import modules.ktwin.event as kevent
 import modules.ktwin.eventstore as keventstore
+import modules.ktwin.twingraph as ktwingraph
 import modules.ktwin.command as kcommand
 
 if os.getenv("ENV") == "local":
@@ -18,6 +19,8 @@ handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s -
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
 
+ktwin_graph = ktwingraph.load_twin_graph()
+
 @app.route("/", methods=["POST"])
 def home():
     event = kevent.handle_request(request)
@@ -26,13 +29,18 @@ def home():
         f"Event TwinInstance: {event.twin_instance} - Event TwinInterface: {event.twin_interface}"
     )
 
-    kevent.handle_event(request, 'air-quality-observed', handle_neighborhood)
+    try:
+        kcommand.handle_command(request=request, twin_interface='s4city-city-neighbourhood', command='updateairqualityindex', twin_graph=ktwin_graph, callback=handle_update_air_quality_index)
+    except Exception as error:
+        app.logger.error(f"Error to handle command updateairqualityindex in TwinInstance {event.twin_instance}")
+        app.logger.error(error)
 
     # Return 204 - No-content
     return "", 204
 
-def handle_neighborhood(event: kevent.KTwinEvent):
-    air_quality_observed = event.cloud_event.data
+def handle_update_air_quality_index(command_event: kcommand.KTwinCommandEvent, target_twin_instance: ktwingraph.TwinInstanceReference):
+    app.logger.info(command_event)
+    app.logger.info(target_twin_instance)
 
 if __name__ == "__main__":
     app.logger.info("Starting up server...")
