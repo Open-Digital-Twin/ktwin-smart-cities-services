8fbb7fced3e97e4b3af46248ef2d544664681ef4
agwermann <alexandrewermann@gmail.com>
2024-04-21 18:19:24 -0300

    chore: add push command

--- golang/build-push-all.sh
+++ golang/build-push-all.sh
@@ -7,9 +7,9 @@ docker buildx build -f Dockerfile -t ghcr.io/open-digital-twin/ktwin-pole-servic
 docker buildx build -f Dockerfile -t ghcr.io/open-digital-twin/ktwin-streetlight-service:0.1 --build-arg SERVICE_NAME=streetlight-service .
 
 # # Push
-# docker push ghcr.io/open-digital-twin/ktwin-device-service:0.1
-# docker push ghcr.io/open-digital-twin/ktwin-neighborhood-service:0.1
-# docker push ghcr.io/open-digital-twin/ktwin-parking-service:0.1
-# docker push ghcr.io/open-digital-twin/ktwin-parking-spot-service:0.1
-# docker push ghcr.io/open-digital-twin/ktwin-pole-service:0.1
-# docker push ghcr.io/open-digital-twin/ktwin-streetlight-service:0.1
+docker push ghcr.io/open-digital-twin/ktwin-device-service:0.1
+docker push ghcr.io/open-digital-twin/ktwin-neighborhood-service:0.1
+docker push ghcr.io/open-digital-twin/ktwin-parking-service:0.1
+docker push ghcr.io/open-digital-twin/ktwin-parking-spot-service:0.1
+docker push ghcr.io/open-digital-twin/ktwin-pole-service:0.1
+docker push ghcr.io/open-digital-twin/ktwin-streetlight-service:0.1
