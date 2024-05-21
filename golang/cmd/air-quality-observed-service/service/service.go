ddaa02554db7f621b0ad1a62de08758885b7494e
agwermann <alexandrewermann@gmail.com>
2024-05-20 21:20:16 -0300

    fix: fix load twingraph

--- golang/cmd/air-quality-observed-service/service/service.go
+++ golang/cmd/air-quality-observed-service/service/service.go
@@ -28,7 +28,7 @@ var twinGraph *ktwin.TwinGraph
 func loadTwinGraph() error {
 	if twinGraph == nil {
 		var err error
-		graph, err := ktwingraph.LoadTwinGraphByInstances([]string{TWIN_INTERFACE_AIR_QUALITY_OBSERVED})
+		graph, err := ktwingraph.LoadTwinGraphByInstances([]string{TWIN_INTERFACE_CITY_POLE})
 		if err != nil {
 			logger.Error("Error loading twin graph", err)
 			return err
